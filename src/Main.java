6ec12c282d0b3f6f0e348e5bdfab5f68cbd9f981
lelam1899 <kylix.par@gmail.com>
2019-01-19 19:12:44 +0700

    dbsanm

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,13 @@
 public class Main {
+    public int he_so;
+    public tong, hieu, tich, thuong ;
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+
+
+        Main y = new Main();
+        y.setHe_so();
+        
     }
 }
